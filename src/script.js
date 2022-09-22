d815a2c311774ec31f9097645608f664d1a637c8
sophbradley <111533096+sophbradley@users.noreply.github.com>
2022-09-22 18:53:50 +0100

    Added API call, inserted returned data to innerHTML elements

--- src/script.js
+++ src/script.js
@@ -1,6 +1,26 @@
-//function displayMovie(event) {
-// event.preventDefault();
-//}
+function displayMovie(response) {
+  let movieTitle = document.querySelector("#movie-title");
+  let releaseYear = document.querySelector("#release-year");
+  let rating = document.querySelector("#rating");
+  let releaseDate = document.querySelector("#release-date");
+  let runtime = document.querySelector("#runtime");
+  let genre = document.querySelector("#genre");
+  let actors = document.querySelector("#actors");
+  let plot = document.querySelector("#plot");
+  // let movieImage = document.querySelector("#poster");
+
+  movieTitle.innerHTML = response.data.Title;
+  releaseYear.innerHTML = response.data.Year;
+  rating.innerHTML = response.data.Rated;
+  releaseDate.innerHTML = response.data.Released;
+  runtime.innerHTML = response.data.Runtime;
+  genre.innerHTML = response.data.Genre;
+  actors.innerHTML = response.data.Actors;
+  plot.innerHTML = response.data.Plot;
+  //posterUrl = response.data.Poster;
+  //console.log(posterUrl);
+  // movieImage.setAttribute("href", response.data.Poster);
+}
 
 function search(movie) {
   let apiId = `30ea62ca`;
@@ -8,11 +28,8 @@ function search(movie) {
   let movieName = document.querySelector("#movie-name").value;
   var formattedMovie = movieName.replace(/ /g, "+");
   let apiUrl = `${apiEndpoint}apikey=${apiId}&t=${formattedMovie}`;
-  console.log(apiUrl);
-  //axios.get(apiUrl).then(displayMovie);
+  axios.get(apiUrl).then(displayMovie);
 }
-
-//http://www.omdbapi.com/?apikey=30ea62ca&t=The+Mummy
 
 function handleSubmit(event) {
   event.preventDefault();
@@ -22,3 +39,5 @@ function handleSubmit(event) {
 
 let searchMovie = document.querySelector("#search-form");
 searchMovie.addEventListener("click", handleSubmit);
+
+//search("The Mummy");
